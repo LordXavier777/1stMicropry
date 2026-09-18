1d43d69a9cb8ea0a6d63e723d5e2ed238e0399b9
LordXavier777 <maekato.86@gmail.com>
2026-09-18 18:16:41 -0500

    Se agregó la entidad "Temporizador extra"
    Es el segundo contador, encargado de contar el tiempo pasados los 35 segundos.

---
+++ Temporizador_extra.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity Temporizador_extra is
+	port(clk_1s: in std_logic;
+	     estado: in std_logic;
+		  en_alarm: in std_logic;
+		  t_extra: out integer range 0 to 99);
+end entity;
+
+architecture temp_xtra of Temporizador_extra is
+	signal count: integer range 0 to 99 :=0;
+begin
+	process(clk_1s)
+	begin
+		if estado='0' then
+			count<=0;
+		else
+			if en_alarm='1' then
+				if count<99 then
+					count<= count + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+t_extra<= count;
+
+end architecture;
