50970564ef29fa1a7ad890a7b72b2c6d610eae01
LordXavier777 <maekato.86@gmail.com>
2026-09-20 16:34:58 -0500

    se corrigió un nombre dentro del test bench

--- Sistema_3/tb_temp_one_button.vhd
+++ Sistema_3/tb_temp_one_button.vhd
@@ -7,7 +7,7 @@ entity tb_temp_one_button is
   
 end entity;
 
-architecture try_out of tb_temp_one is
+architecture try_out of tb_temp_one_button is
   
   component Temp_one_button is
     port( clk,btn: in std_logic;
@@ -23,5 +23,69 @@ architecture try_out of tb_temp_one is
 begin
   DUT: Temp_one_button port map(
       clk => clk_tb,
-      
+      btn => btn_tb,
+      sec_min => sec_min_tb,
+      sec_dec => sec_dec_tb,
+      sec_uni => sec_uni_tb);
+    
+  clk_process: process
+  begin
+    clk_tb <= '0';
+        wait for clk_period/2;
+        clk_tb <= '1';
+        wait for clk_period/2;
+    end process;
+    
+  stimulus: process
+    begin
+        -- =========================================================
+        -- CASO 1: Condición Inicial (Sistema en reposo)
+        -- =========================================================
+        btn_tb <= '1'; -- Botón sin presionar
+        wait for 30 ns; 
+        
+        -- =========================================================
+        -- CASO 2: Arranque (Toque corto de menos de 2 ciclos)
+        -- =========================================================
+        btn_tb <= '0'; -- Simulamos el clic del usuario[cite: 11]
+        wait for 10 ns; -- Lo mantenemos 1 ciclo (1 segundo virtual)
+        btn_tb <= '1'; -- Soltamos el botón rápido[cite: 11]
+        
+        -- Dejamos que el reloj avance libremente para ver el conteo
+        wait for 150 ns; 
+
+        -- =========================================================
+        -- CASO 3: Pausa (Otro toque corto)
+        -- =========================================================
+        btn_tb <= '0'; 
+        wait for 10 ns; 
+        btn_tb <= '1'; 
+        
+        -- Dejamos pasar el tiempo para confirmar que el contador está congelado
+        wait for 50 ns;
+
+        -- =========================================================
+        -- CASO 4: Reanudación
+        -- =========================================================
+        btn_tb <= '0'; 
+        wait for 10 ns;
+        btn_tb <= '1'; 
+        
+        wait for 100 ns;
+
+        -- =========================================================
+        -- CASO 5: Reinicio general (Toque largo mayor a 2 ciclos)
+        -- =========================================================
+        btn_tb <= '0'; 
+        -- Lo mantenemos presionado por 30 ns (equivale a 3 segundos virtuales)
+        
+        wait for 30 ns; 
+        btn_tb <= '1';
+
+        wait for 50 ns;
+
+        wait; 
+    end process;
+
+end architecture;
   
